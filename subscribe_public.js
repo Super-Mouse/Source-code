7956425b5a7749f91e1754697b695236cd6a5745
Super-Mouse <349790096@qq.com>
2018-05-10 10:47:37 +0800

    修正订阅发布模式的bug 事件on

--- subscribe_public.js
+++ subscribe_public.js
@@ -1,34 +1,33 @@
-var Event = {
-  handlers : {},
-  on (eventName, callback){
-    var currentEvent = this.handlers[eventName];
-    if (!currentEvent) {
-      currentEvent = [];
-    }
-    currentEvent.push(callback);
-  },
-  off (eventName, callback){
-    var currentEvent = this.handlers[eventName];
-    if (currentEvent) {
-      if (!callback) { //没有传callback，表示解除所有该事件的绑定函数
-        currentEvent.length = 0;
-      } else{
-        for (var i = 0; i < current.length; i++) {
-          if (currentEvent[i] == callback) { //疑问：两个函数能否比较？
-            currentEvent.splice(i, 1);
-          }
+let Event = {
+    handlers : {},
+    on (eventName, callback){
+        if (!this.handlers[eventName]) {
+            this.handlers[eventName] = [];
         }
-      }
-    }
-  },
-  fire (eventName, data){
-    var currentEvent = this.handlers[eventName],
-        param = Array.prototype.slice(arguments, 1);
-    if (currentEvent) {
-      for (var i = 0; i < currentEvent.length; i++) {
-        currentEvent[i](param);
-      }
-    }
+        this.handlers[eventName].push(callback);
+    },
+    off (eventName, callback){
+        let currentEvent = this.handlers[eventName];
+        if (currentEvent) {
+            if (!callback) { //没有传callback，表示解除所有该事件的绑定函数
+                currentEvent.length = 0;
+            } else{
+                for (let i = 0; i < currentEvent.length; i++) {
+                    if (currentEvent[i] === callback) {
+                        currentEvent.splice(i, 1);
+                    }
+                }
+            }
+        }
+    },
+    fire (eventName, data){
+        let currentEvent = this.handlers[eventName],
+            param = Array.prototype.slice(arguments, 1);
+        if (currentEvent) {
+            for (let i = 0; i < currentEvent.length; i++) {
+                currentEvent[i](param);
+            }
+        }
 
-  }
+    }
 }
