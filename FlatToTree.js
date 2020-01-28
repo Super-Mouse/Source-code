aef6628f442bf81edf9bfdad8ab197dc6ba4ae4c
fenghao <fenghao@vipkid.com.cn>
2020-01-28 22:25:31 +0800

    扁平结构转树型结构

--- FlatToTree.js
+++ FlatToTree.js
@@ -1,3 +1,20 @@
 function toTree(arr) {
-
-}
+  let temp = {}
+  let obj = {}
+  for (const item of object) {
+    temp[item.id] = item
+  }
+  for (const key in temp) {
+    if (temp.hasOwnProperty(key)) {
+      if (temp[key].pid) {
+        if(!obj[temp[key].pid].children){
+          obj[temp[key].pid].children = {}
+        }
+        obj[temp[key].pid].children[temp[key].id] = temp[key]
+      } else {
+        obj[temp[key].id] = temp[key]
+      }
+    }
+  }
+  return obj
+} 
